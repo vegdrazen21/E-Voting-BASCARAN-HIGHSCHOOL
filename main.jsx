b17edc40343f23060cef1e6831e48f43acae8a7c
Lee-stud <lunalee694@gmail.com>
2025-09-02 20:39:58 +0800

    main.jsx

---
+++ main.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.css";
+
+createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
